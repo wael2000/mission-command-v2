5f59caaca7bd4e9a0de4aa97f6bd959c760cd348
wael2000 <weldoami@redhat.com>
2024-09-16 19:17:35 +0200

    Done

--- gitops/scripts/demo.sh
+++ gitops/scripts/demo.sh
@@ -1,12 +1,12 @@
-DC_URL=https://api.cluster-cmlmj.cmlmj.sandbox2812.opentlc.com:6443
+DC_URL=https://api.cluster-d2npd.d2npd.sandbox2068.opentlc.com:6443
 DC_UID=admin
-DC_PWD=7ZZsJ44VxlTzpbP9
+DC_PWD=CnR3qicE4mzQ5sm5
 #DC_TOKEN=eyJhbGciOiJSUzI1NiIsImtpZCI6IkxUeEp5cXdUdnRGOXBvZzdPckxWR1BSVWZMa2N2T2xFRi10eHdIYTF3aVUifQ.eyJpc3MiOiJrdWJlcm5ldGVzL3NlcnZpY2VhY2NvdW50Iiwia3ViZXJuZXRlcy5pby9zZXJ2aWNlYWNjb3VudC9uYW1lc3BhY2UiOiJodWItbnMiLCJrdWJlcm5ldGVzLmlvL3NlcnZpY2VhY2NvdW50L3NlY3JldC5uYW1lIjoicmhzaS10b2tlbi1qc3I0biIsImt1YmVybmV0ZXMuaW8vc2VydmljZWFjY291bnQvc2VydmljZS1hY2NvdW50Lm5hbWUiOiJyaHNpIiwia3ViZXJuZXRlcy5pby9zZXJ2aWNlYWNjb3VudC9zZXJ2aWNlLWFjY291bnQudWlkIjoiNDZiMGJkMTgtYTc5Yy00ZTkwLWJlY2MtY2Y4NWQ0MjBmNGZjIiwic3ViIjoic3lzdGVtOnNlcnZpY2VhY2NvdW50Omh1Yi1uczpyaHNpIn0.QBNsLXWKFs0_AZnAqRMcsqJYTbunQ1AQye6NZ6Vtd9C2s8z87VDRnqbq8MAmwb9q86tCNYu7n2UccB9URWtSIqx3td8KjI59RKv8Ju4BGGa-q943d9RRRNGcYt1EuStv-IaOO-eeFt1gchwUxZOUjMMxcI7Kfmb1nJkvLEzOKq1SHQbFcC8bsWtZgpNedasUNURI-s1QN3Nwn-EpvpBYBL2uk2wyhhz_6PxfMyFbLYmKmIiOHr47a4TVysHviZGdjmFZspZhIIUvq-VzGl3x0RtM3uuzIHQnPvcG0gwvEavjohR1sf6GtbVfwyI2_18WlzCgliQZzS2IDVd8gM2yDpLruunVt-hVUd3jmExMsB8T178NzQVpOJYRC-5EaULUQBCBQ-pTncWiPaMUgP4vvmUVlWUNkIAR2aSWaW0BbL-r579tx9OF1El0BtOt4z7gDotjKr5zAR8t8Ef5_AqCy3cqF18L-XqCnNX67AV_RzSf4ZC5QkvzUCC8xDqmvxVjRtzmjNPIOo6spjJ1t-x7kAccnpSUZe0BP1Xrz83ysrdYMjGClEXjP9AAuNFaWbmJiR2U0Wo1PqyKZN_-9Y5_ca3ObwXUNE9xPOPo5-T_Fua3W74CSSltEwMnqh-iXe0yfSu1oOnfx0JErSS-3loO4K0iDET1a7f9tqaxNDuHrPc
 DC_STATUS=on  # on/off
 
-AWS_URL=https://api.cluster-j874s.j874s.sandbox2998.opentlc.com:6443
-AWS_UID=kubeadmin
-AWS_PWD=dQpyc-rosni-Xvtdv-E7Bud
+AWS_URL=https://api.cluster-98d76.98d76.sandbox932.opentlc.com:6443
+AWS_UID=admin
+AWS_PWD=NDgiKCr2eMIOQczD
 AWS_TOKEN=
 AWS_STATUS=on   # on/off
 
@@ -18,9 +18,9 @@ AZURE_STATUS=off
 
 AZURE_NATIVE_STATUS=on
 
-PROJECT=pss
+PROJECT=fox-team
 
-MSG="Welcome to Red Hat Dora Demo"
+MSG="Welcome to Red Hat Demo"
 RESULT=""
 
 alias skupper=~/Downloads/skupper
@@ -74,11 +74,11 @@ print_fixed_line
 echo "                              ┏   ┏┓        ┓┏  ┓   • ┓  ┏┓┓     ┓   ┓"
 echo "           _\|/_             ━┫   ┃┃┏┓┏┓┏┓  ┣┫┓┏┣┓┏┓┓┏┫  ┃ ┃┏┓┓┏┏┫   ┣━"
 echo "           (o o)              ┗   ┗┛┣┛┗ ┛┗  ┛┗┗┫┗┛┛ ┗┗┻  ┗┛┗┗┛┗┻┗┻   ┛"
-echo " ┏━━━━━━oOO.{-}.OOo━━━━━━━┳━━━━━━━━━┛━━━━━━━━━━┛━━━━┳━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━[Capters]━┓"
-echo " ┃  ╦═╗╔═╗╔╦╗  ╦ ╦╔═╗╔╦╗  ┃      ╔╦╗╔═╗╦═╗╔═╗       ┃ \033[4m\033[1;37mc1\033[0m Risk Management       \033[4m\033[1;37mc2\033[0m Incident Reports     ┃"
-echo " ┃  ╠╦╝║╣  ║║  ╠═╣╠═╣ ║   ┃       ║║║ ║╠╦╝╠═╣       ┃ \033[4m\033[1;37mc3\033[0m Resilience Testing    \033[4m\033[1;37mc4\033[0m Third Party Risk     ┃"
-echo " ┃  ╩╚═╚═╝═╩╝  ╩ ╩╩ ╩ ╩   ┃      ═╩╝╚═╝╩╚═╩ ╩       ┃ \033[4m\033[1;37mc5\033[0m Information sharing                           ┃"
-echo " ┣━━━━━━━━━━━━━━━━━━━━━━━━╋━━━━━━━━━━━━━━━━━━━━━━━━━╋━━━━━━━━━━━━━━━━━━━━━┳━━━━━━━━━━━━━━━━━━━━━━━━━━━━┫"
+echo " ┏━━━━━━oOO.{-}.OOo━━━━━━━━━━━━━━━━━┛━━━━━━━━━━┛━━━━┳━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━[ Scope ]━━┓"
+echo " ┃      ╦═╗╔═╗╔╦╗  ╦ ╦╔═╗╔╦╗                        ┃   Creating a common fabic to deliver resilient   ┃"
+echo " ┃      ╠╦╝║╣  ║║  ╠═╣╠═╣ ║                         ┃   national services with seamless experience     ┃"
+echo " ┃      ╩╚═╚═╝═╩╝  ╩ ╩╩ ╩ ╩                         ┃   acrross all infrastrucutre providers.          ┃"
+echo " ┣━━━━━━━━━━━━━━━━━━━━━━━━┳━━━━━━━━━━━━━━━━━━━━━━━━━╋━━━━━━━━━━━━━━━━━━━━━╋━━━━━━━━━━━━━━━━━━━━━━━━━━━━┫"
 echo " ┃  ┳┓┏┓                  ┃  ┏┓┓ ┏┏┓                ┃  ┏┓┏┓┳┳┳┓┏┓         ┃  ┏┓┏┓┳┳┳┓┏┓  ┳┓┏┓┏┳┓┳┓┏┏┓  ┃"
 echo " ┃  ┃┃┃                   ┃  ┣┫┃┃┃┗┓                ┃  ┣┫┏┛┃┃┣┫┣          ┃  ┣┫┏┛┃┃┣┫┣   ┃┃┣┫ ┃ ┃┃┃┣   ┃"
 echo " ┃  ┻┛┗┛                  ┃  ┛┗┗┻┛┗┛                ┃  ┛┗┗┛┗┛┛┗┗┛         ┃  ┛┗┗┛┗┛┛┗┗┛  ┛┗┛┗ ┻ ┻┗┛┗┛  ┃"
@@ -152,7 +152,7 @@ fi
 # DC Bind
 oc login --server=$DC_URL -u $DC_UID -p $DC_PWD --insecure-skip-tls-verify=true
 oc project $PROJECT
-skupper service bind rtgs-apis service rtgs-apis-rhsi
+skupper service bind fixed-wing-apis service fixed-wing-apis-rhsi
 skupper service bind postgresql service postgresqldb
 menu_array["5"]="\033[43m5) bind service\033[0m"
 menu_array["9"]="9) unbind service"
@@ -162,7 +162,7 @@ menu_array["9"]="9) unbind service"
 # bind service on AWS
 oc login --server=$AWS_URL -u $AWS_UID -p $AWS_PWD --insecure-skip-tls-verify=true
 oc project $PROJECT
-skupper service bind rtgs-apis service rtgs-apis-rhsi
+skupper service bind fixed-wing-apis service fixed-wing-apis-rhsi
 skupper service bind postgresql service postgresqldb
 menu_array["6"]="\033[43m6) bind service\033[0m"
 menu_array["10"]="10) unbind service"
@@ -172,7 +172,7 @@ menu_array["10"]="10) unbind service"
 # bind service on Azure
 oc login --server=$AZURE_URL -u $AZURE_UID -p $AZURE_PWD --insecure-skip-tls-verify=true
 oc project $PROJECT
-skupper service bind rtgs-apis service rtgs-apis-rhsi
+skupper service bind fixed-wing-apis service fixed-wing-apis-rhsi
 skupper service bind postgresql service postgresqldb
 menu_array["7"]="\033[43m7) bind service\033[0m"
 menu_array["11"]="11) unbind service"
@@ -183,7 +183,7 @@ menu_array["11"]="11) unbind service"
 oc login --server=$DC_URL -u $DC_UID -p $DC_PWD --insecure-skip-tls-verify=true
 # api from dc
 oc project $PROJECT
-skupper service bind rtgs-apis service rtgs-apis-rhsi
+skupper service bind fixed-wing-apis service fixed-wing-apis-rhsi
 # db from Azure native
 oc project $PROJECT-azure
 skupper service bind postgresql service azure-postgresql-service
@@ -198,7 +198,7 @@ menu_array["12"]="12) unbind service"
 # unbind the service in DC to see the impact
 oc login --server=$DC_URL -u $DC_UID -p $DC_PWD --insecure-skip-tls-verify=true
 oc project $PROJECT
-skupper service unbind rtgs-apis service rtgs-apis-rhsi
+skupper service unbind fixed-wing-apis service fixed-wing-apis-rhsi
 skupper service unbind postgresql service postgresqldb
 menu_array["9"]="\033[43m9) unbind service\033[0m"
 menu_array["5"]="5) bind service"
@@ -217,7 +217,7 @@ menu_array["6"]="6) bind service"
 # unbind service on Azure
 oc login --server=$AZURE_URL -u $AZURE_UID -p $AZURE_PWD --insecure-skip-tls-verify=true
 oc project $PROJECT
-skupper service unbind rtgs-apis service rtgs-apis-rhsi
+skupper service unbind fixed-wing-apis service fixed-wing-apis-rhsi
 skupper service unbind postgresql service postgresqldb
 menu_array["11"]="\033[43m11) unbind service\033[0m"
 menu_array["7"]="7) bind service"
@@ -264,11 +264,11 @@ fi
 
 "e")
 # DC : Expose db service
-# Make sure you remove local svc for postgresql and rtgs-apis
+# Make sure you remove local svc for postgresql and fixed-wing-apis
 oc login --server=$DC_URL -u $DC_UID -p $DC_PWD --insecure-skip-tls-verify=true
 oc project $PROJECT
 skupper service create postgresql 5432 --protocol tcp
-skupper service create rtgs-apis 8080 --protocol http
+skupper service create fixed-wing-apis 8080 --protocol http
 ;;
 
 "d")
@@ -300,24 +300,6 @@ then
   skupper delete
 fi
 
-;;
-
-
-
-"c1")
-MSG="[c1] Business continuity policies and disaster recovery plans as a C-level responsibility"
-;;
-"c2")
-MSG="[c2] Harmonization of mandatory incident reporting frameworks (e.g. GDPR, NIS, eIDAs, SSM, PSD2)"
-;;
-"c3")
-MSG="[c3] At least annually, including remediation plans with ICT third party providers direct involvement"
-;;
-"c4")
-MSG="[c4] Critical ICT Third-Parties subject to an EU oversight framework under a financial European Supervisory Authority"
-;;
-"c5")
-MSG="[c5] Voluntary exchange of threat information & Intelligence are allowed and supported"
 ;;
 
 esac
